5d5c17a0ca5d1b16707cc128ff0aa9ecccd47ffc
AppIFC748 <70822994+AppIFC748@users.noreply.github.com>
2021-07-18 20:00:43 -0300

    Agenda

--- app/src/main/java/com/ifcbrusque/app/fragments/home/InsereTarefas.java
+++ app/src/main/java/com/ifcbrusque/app/fragments/home/InsereTarefas.java
@@ -1,36 +1,34 @@
 package com.ifcbrusque.app.fragments.home;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ifcbrusque.app.R;
 
-public class HomeFragment extends Fragment {
-
-    private HomeViewModel homeViewModel;
+public class InsereTarefas extends Fragment {
 
 
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        FloatingActionButton inserir = root.findViewById(R.id.Inserir);
-        inserir.setOnClickListener(new View.OnClickListener() {
+
+        View root = inflater.inflate(R.layout.fragment_insere_tarefas, container, false);
+        FloatingActionButton volta = root.findViewById(R.id.volta);
+        volta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.nav_host_fragment,new InsereTarefas())
+                        .replace(R.id.nav_host_fragment,new HomeFragment())
                         .commit();
             }
         });
@@ -39,4 +37,3 @@ public class HomeFragment extends Fragment {
         return root;
     }
 }
-
