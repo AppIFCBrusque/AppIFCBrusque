34c2afea526eef4c10fb58c857188db32d653dd9
stckd <fabricio.duarte.jr@gmail.com>
2022-02-20 18:28:34 -0300

    Animação ao trocar de fragmento

--- app/src/main/java/com/ifcbrusque/app/ui/home/HomeActivity.java
+++ app/src/main/java/com/ifcbrusque/app/ui/home/HomeActivity.java
@@ -5,12 +5,11 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ifcbrusque.app.R;
 import com.ifcbrusque.app.ui.base.BaseActivity;
+import com.ifcbrusque.app.ui.home.lembretes.LembretesFragment;
+import com.ifcbrusque.app.ui.home.noticias.NoticiasFragment;
 
 import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.Fragment;
 
 public class HomeActivity extends BaseActivity {
 
@@ -26,16 +25,42 @@ public class HomeActivity extends BaseActivity {
 
     @Override
     protected void setUp() {
+        //Configuração da toolbar
         Toolbar toolbar = findViewById(R.id.toolbarHome);
+        toolbar.setTitle(R.string.title_home);
         setSupportActionBar(toolbar);
 
+        //Configuração do bottom navigation view
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations. (remove o botao de voltar na barra superior se for topo)
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_noticias).build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        navView.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            int idTitulo = R.string.app_name;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new LembretesFragment();
+                    idTitulo = R.string.title_home;
+                    break;
+
+                case R.id.navigation_noticias:
+                    fragment = new NoticiasFragment();
+                    idTitulo = R.string.title_noticias;
+                    break;
+            }
+
+            if (fragment != null) {
+                //Trocar para o fragmento
+                getSupportFragmentManager().beginTransaction()
+                        .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+                        .add(R.id.nav_host_fragment, fragment)
+                        .commit();
+                toolbar.setTitle(idTitulo);
+                return true;
+            } else {
+                return false; //Retornar false não troca o item selecionado no bottom navigation view
+            }
+        });
+
     }
 
 }
