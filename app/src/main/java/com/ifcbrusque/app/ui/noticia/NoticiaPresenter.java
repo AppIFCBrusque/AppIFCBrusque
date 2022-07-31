211628713958124feb0e2b8ffed9e028d153b2a9
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2022-07-31 18:27:58 -0300

    Corrigir identificação de notícia do SIGAA/campus

--- app/src/main/java/com/ifcbrusque/app/ui/noticia/NoticiaPresenter.java
+++ app/src/main/java/com/ifcbrusque/app/ui/noticia/NoticiaPresenter.java
@@ -21,6 +21,7 @@ import com.ifcbrusque.app.ui.base.BasePresenter;
 import javax.inject.Inject;
 
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import timber.log.Timber;
 
 public class NoticiaPresenter<V extends NoticiaContract.NoticiaView> extends BasePresenter<V> implements NoticiaContract.NoticiaPresenter<V> {
     @Inject
@@ -32,6 +33,7 @@ public class NoticiaPresenter<V extends NoticiaContract.NoticiaView> extends Bas
      * Utilizado para carregar uma notícia (do banco de dados ou da internet), armazenar e mostrar na view
      */
     private void carregarNoticiaArmazenada(Preview preview) {
+        Timber.i("Tentando carregar notícia armazenada");
         getCompositeDisposable().add(getDataManager()
                 .getNoticia(preview.getUrlNoticia())
                 .subscribe(noticia -> exibirNoticiaNaView(preview, noticia),
@@ -39,6 +41,7 @@ public class NoticiaPresenter<V extends NoticiaContract.NoticiaView> extends Bas
     }
 
     private void carregarNoticiaWeb(Preview preview) {
+        Timber.i("Carregando notícia da web");
         getCompositeDisposable().add(getDataManager()
                 .getNoticiaWeb(preview)
                 .flatMap(noticia -> {
@@ -72,19 +75,20 @@ public class NoticiaPresenter<V extends NoticiaContract.NoticiaView> extends Bas
 
     @Override
     public void onViewPronta(Bundle bundle) {
-        String url = bundle.getString(NOTICIA_URL_IMAGEM_PREVIEW);
+        String url = bundle.getString(NOTICIA_URL);
 
         if (url.length() > 0) {
             // Notícia do campus
+            Timber.i("Notícia do campus");
             Preview preview = new Preview(bundle.getString(NOTICIA_TITULO),
                     "",
                     bundle.getString(NOTICIA_URL_IMAGEM_PREVIEW),
                     bundle.getString(NOTICIA_URL),
                     fromTimestamp(bundle.getLong(NOTICIA_DATA)));
-
             carregarNoticiaArmazenada(preview);
         } else {
             // Notícia do SIGAA
+            Timber.i("Notícia do SIGAA");
             getMvpView().setTitulo(bundle.getString(NOTICIA_TITULO));
             getMvpView().setData(bundle.getString(NOTICIA_DATA));
             getMvpView().setDisciplina(bundle.getString(NOTICIA_NOME_DISCIPLINA));
